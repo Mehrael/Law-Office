ae61307c36658529e46f95141338a346b3eed2ec
Mehrael <mehraelashraf20@gmail.com>
2022-09-29 09:41:34 +0200

    sidebar toggle

--- shared/navbar.php
+++ shared/navbar.php
@@ -3,7 +3,9 @@
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="/ODC8/dashboard.php"><i class="fa-solid fa-scale-balanced"></i> Law Office</a>
     <!-- Sidebar Toggle-->
-    <!-- <button class="btn btn-link  btn-sm order-1 order-lg-0 me-4 me-lg-0 " id="sidebarToggle" type=""><i class="fas fa-bars"></i></button> -->
+    <button class="btn btn-link  btn-sm order-1 order-lg-0 me-4 me-lg-0 " id="sidebarToggle" type="">
+        <i class="fas fa-bars"></i>
+    </button>
     <!-- Sized Box -->
     <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
     </div>
